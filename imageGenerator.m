da1d33befac0d44dee803f28a21e95913710ba24
Yusuke710 <58585405+Yusuke710@users.noreply.github.com>
2021-07-09 14:29:42 +1000

    Add files via upload
    square image

--- imageGenerator.m
+++ imageGenerator.m
@@ -11,8 +11,8 @@ for i = 1:images;
     numPoints = input(prompt)
 
     % make the figure full screen
-    FigH = figure('Position', get(0, 'Screensize'),'visible','off'); %remove 'visible','off'if you would like to display the figure
-    
+    %FigH = figure('Position', get(0, 'Screensize'),'visible','off'); %remove 'visible','off'if you would like to display the figure
+    FigH = figure('Position',[400 400 400 400],'visible','off');
     % generate dots
     x = rand(1, numPoints);
     y = rand(1, numPoints);
@@ -21,11 +21,11 @@ for i = 1:images;
     plot(x, y, 'k.','MarkerSize',sz);
     pbaspect([1 1 1]) % control the ratio of the axis, it is square here
     
-    % make plot background black
+    % make plot background white
     set(gca,'Color','w')
     % remove axis in plot
     set(gca,'XColor','none','YColor','none','TickDir','out') 
-    % make background black
+    % make background white
     set(gcf,'color','w')
 
     % save images
